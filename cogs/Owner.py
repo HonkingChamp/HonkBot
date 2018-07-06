6b2a3f20cefad22e1be049e1c3f777925ed58041
HonkingChamp <syleproductions@gmail.com>
2018-07-07 06:16:39 +0800

    Update Owner.py

--- cogs/Owner.py
+++ cogs/Owner.py
@@ -54,13 +54,38 @@ class OwnerCog:
     @commands.is_owner()
     async def leave(self, ctx):
         await ctx.guild.leave()
+    
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        try:
+            await ctx.send('Shutting down')
+        except HTTPException:
+            pass
+
+        try:
+            pending = asyncio.Task.all_tasks(loop=self.bot.loop)
+            gathered = asyncio.gather(*pending, loop=self.bot.loop)
+            try:
+                gathered.cancel()
+                self.bot.loop.run_until_complete(gathered)
+
+                # we want to retrieve any exceptions to make sure that
+                # they don't nag us about it being un-retrieved.
+                gathered.exception()
+            except:
+                pass
+
+        except:
+            pass
+        finally:
+            await self.bot.logout()
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def update_bot(self, ctx, *, options=None):
+     async def update_bot(self, ctx, *, options=None):
         """Does a git pull"""
         cmd = 'git pull'.split(' ')
-        self.bot.threadpool = ThreadPoolExecutor(max_workers=2)
         if options:
             cmd.extend(shlex.split(options))
 
@@ -70,10 +95,28 @@ class OwnerCog:
         if err:
             out = err.decode('utf-8') + out
 
+        files = re.findall(r'(cogs/\w+)(?:.py *|)', out)
+
         if len(out) > 2000:
             out = out[:1996] + '...'
 
         await ctx.send(out)
+
+        if files:
+            files = [f.replace('/', '.') for f in files]
+            await ctx.send(f'Do you want to reload files `{"` `".join(files)}`')
+            def do_reload():
+               messages = []
+               for file in files:
+                   try:
+                       self.bot.unload_extension(file)
+                       self.bot.load_extension(file)
+                   except Exception as e:
+                       messages.append('Failed to load extension {}\n{}: {}'.format(file, type(e).__name__, e))
+                   else:
+                       messages.append(f'Reloaded {file}')
+
+               return messages
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
